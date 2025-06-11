547c71992edaae95e9626962e8141dace38d27ce
Natalia <natalia.carrasco2101@alumnos.ubiobio.cl>
2025-06-10 23:51:46 -0400

    Front carrito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './product/product-list/product-list.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { CarritoListarComponent } from './carrito/carrito-listar/carrito-listar.component';
 
 export const routes: Routes = [
   { path: 'product/product-list', component: ProductListComponent },
   { path: 'welcome', component: WelcomeComponent },
+  { path: 'carrito/carrito-listar', component: CarritoListarComponent}
 ];
---
+++ src/app/carrito/carrito-listar/carrito-listar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrito-listar',
+  standalone: true,
+  imports: [],
+  templateUrl: './carrito-listar.component.html',
+  styleUrl: './carrito-listar.component.css'
+})
+export class CarritoListarComponent {
+
+}
