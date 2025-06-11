124f1b9a401500e8552dcb23c5b49cc6ff71a4f2
Jose Henriquez <jose.henriquez2101@alumnos.ubiobio.cl>
2025-06-10 21:08:33 -0400

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './product/product-list/product-list.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 export const routes: Routes = [
   { path: 'product/product-list', component: ProductListComponent },
-  
+  { path: 'welcome', component: WelcomeComponent },
 ];
